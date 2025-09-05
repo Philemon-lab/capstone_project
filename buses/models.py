5ebfd6110b3eb787276b8f0814bb02e860350dba
Philemon-lab <umpireltd@gmail.com>
2025-09-05 15:11:30 +0200

    feat: Adding route and trip models to buses/models, Route for defining path from origin to destination with intermediate stop and Trip to represent a specific journey on route with a bus at a specific time, also JSONField is used on available_seats to track which seats are availabel

--- buses/models.py
+++ buses/models.py
@@ -29,3 +29,33 @@ class Bus(models.Model):
 
     def __str__(self):
         return f"{self.bus_number} ({self.agency.name})"
+
+class Route(models.Model):
+    name = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    distance = models.DurationField(help_text="Estimated traval time")
+    intermediate_stops = models.JSONField(default=list)
+
+    def __str__(self):
+        return f"{self.origin} to {self.destination}"
+    
+
+class Trip(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE, related_name='trips')
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='trips')
+    departure_time = models.DateTimeField()
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    available_seates = models.JSONField(default=list)
+    status = models.CharField(max_length=20,choices=(
+        ('scheduled', 'Scheduled'),
+        ('boarding', 'Boarding'),
+        ('departed', 'Departed'),
+        ('arrived', 'Arrived'),
+        ('canceled', 'Canceled')
+    ), default='scheduled'
+    )
+
+    def __str__(self):
+        return f"{self.bus.bus_number} - {self.route} -{self.departure_time}"
