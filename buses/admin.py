3b85975872f7d3ba75ed336a7dc239c2daa5da84
Philemon-lab <umpireltd@gmail.com>
2025-09-05 16:24:33 +0200

    Feat: Creating Registering models in admin interface

--- buses/admin.py
+++ buses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BusAgency, Bus, Trip, Trip
 
-# Register your models here.
+admin.site.register(BusAgency)
+admin.site.register(Bus)
+admin.site.register(Route)
+admin.site.register(Trip)
+
